9410df08fc8ee31c780dc2d18e7caf93804bf60d
RyanMConstable <ryanmconstable@gmail.com>
2023-07-24 13:38:54 -0400

    Removed useless code from flask

--- backend/base.py
+++ backend/base.py
@@ -4,17 +4,13 @@ from flask_cors import CORS, cross_origin
 
 api = Flask(__name__)
 
-api.config['SECRET_KEY'] = 'the quick brown fox jumps over the lazy   dog'
-api.config['CORS_HEADERS'] = 'Content-Type'
 
-cors = CORS(api, resources={r"/profile": {"origins": "http://localhost:port"}})
 #Call this from the frontend to get the response_body dictionary
 @api.route('/profile')
 @cross_origin(origin='localhost',headers=['Content- Type', 'Authorization'])
 def my_profile():
     
     topkilluser = c.findTopX("totalkills", 1)
-    print(topkilluser)
     #Here we can change this to whatever object we want
     response_body = {
         "total" : str(topkilluser)
