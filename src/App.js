d7c152087e7ef0dde2750fcaec6e7b7cff045f51
RyanMConstable <ryanmconstable@gmail.com>
2023-07-24 13:28:06 -0400

    Displays data from flask

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function ReturnApp(props) {
   if (props.result == null){
     return(<div>Original</div>)
   } else {
-    console.log("Here", props)
+    console.log(props)
     return(<div>{props.result.about_me}</div>)
   }
 }
@@ -19,44 +19,26 @@ function App() {
   
   //Testing getdata function
   function getData() {
-    console.log("getData")
-    console.log(profileData)
+    console.log("GetData", profileData)
     axios({
       method: "GET",
       url:"http://localhost:5000/profile",
     }).then((response) => {
       const res = response.data
-      console.log(res)
       setProfileData(({
         profile_name: res.name,
         about_me:res.about
       }))
     }).catch((error) => {
       if (error.response) {
-        console.log("ERROR")
       }
     })
   }
 
-  console.log("Here")
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
       <button onClick = {getData}>Test button</button>
-      <ReturnApp result = {profileData}></ReturnApp>
+      <ReturnApp result = {profileData}/>
     </div>
   );
 }
